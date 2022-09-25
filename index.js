2958f943c98c8ef74cb0fc6e9b21fbe2a1a2b5be
Virginia <vbernasconi@altimetrik.com>
2022-09-25 02:51:40 -0300

    Basic API call

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-const API_URL = 'https://jsonplaceholder.typicode.com';
-const HTMLResponse = document.querySelector('#app');
-const ul = document.createElement('ul');
+const aplication = document.querySelector('.container');
+const url = 'https://jsonplaceholder.typicode.com/users';
 
-fetch(`${API_URL}/users`)
-  .then((response) => response.json())
-  .then((users) => {
-    users.forEach((user) => {
-      let elem = document.createElement('li');
-      elem.appendChild(document.createTextNode(`${user.name} ${user.email}`));
-      ul.appendChild(elem);
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((user) => {
+      const p = document.createElement('p');
+      p.innerHTML = user.name;
+      aplication.appendChild(p);
     });
-    HTMLResponse.appendChild(ul);
-    /*  const tpl = users.map((user) => `<li>${user.name} ${user.email}</li>`);
-    HTMLResponse.innerHTML = `<ul>${tpl}</ul>`; */
-  });
-
-/* const xhr = new XMLHttpRequest();
-
-function onRequestHandler() {
-  if (this.readyState === 4 && this.status === 200) {
-    // 0 = UNSET, no se ha llamado al metodo open
-    // 1=open, se ha llamado al metodo open
-    //2= HEADERS_RECEIVED se esta llamando al metodo send()
-    //3= LOADING, está cargando, es decir, está recibiendo la respuesta
-    //4=DONE, se ha completado la operación
-    const data = JSON.parse(this.response);
-    const HTMLResponse = document.querySelector('#app');
-
-    const tpl = data.map((user) => `<li>${user.name} ${user.email}</li>`);
-    HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
-  }
-}
-xhr.addEventListener('load', onRequestHandler);
-xhr.open('GET', `${API_URL}/users`);
-xhr.send(); */
+  })
+  .catch((err) => console.log(err));
